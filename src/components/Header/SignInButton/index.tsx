6aa267bb332693ade91ade54ecd450c6ad7b3f5b
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2021-06-25 09:33:28 -0300

    Add next auth and faunaDB to sign

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,28 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.button}
-      color="#04d361"
+      onClick={() => signOut()}
     >
-      <FaGithub />
-      Ian Carlos
+      <FaGithub color="#04d361" />
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button 
       type="button"
       className={styles.button}
-      color="#eba417"
+      onClick={() => signIn('github')}
     >
-      <FaGithub />
+      <FaGithub  color="#eba417" />
       Sign in with Github
     </button>
   )
